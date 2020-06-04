56fd4b30f1d95d714a4ab0c98869c786d239ff53
shantana81 <shantana.vina@gmail.com>
2020-06-03 19:57:52 -0500

    first commit

---
+++ CoreJavaJune2020/src/JavaBasics/HelloWorld.java
@@ -0,0 +1,12 @@
+package JavaBasics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO i have a pending work to do tonight here
+		// TODO testing another todo
+		System.out.println("Hello World");
+		System.out.println("microphone \ttesting \n1234");
+	}
+
+}
